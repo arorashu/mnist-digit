75bdf2c728335b5a565514e32e94696891a4401e
Shubham Arora <shubhama1903@gmail.com>
2021-08-12 23:27:53 -0700

    created loader file to deserialize and view img

---
+++ README.md
@@ -0,0 +1,8 @@
+# MNIST digit recognizer
+
+
+Neural network based techniques to recognize digits from handwritten images in the MNIST dataset
+
+
+
+
--- loader.py
+++ loader.py
@@ -2,8 +2,6 @@
 deserialize the mnist pickle and view photos
 """
 
-# import matplotlib
-# matplotlib.use('TkAgg')
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import pickle
@@ -11,12 +9,12 @@ import pickle
 
 if __name__ == "__main__":
   
-  with open('mnist.pkl', 'rb') as f:
+  with open('data/mnist.pkl', 'rb') as f:
     train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 
   train_x, train_y = train_set
-  print(f'train_x size: {len(train_x)}')
-  plt.imshow(train_x[2].reshape((28, 28)), cmap=cm.Greys_r)
+  # print(f'train_x size: {len(train_x)}')
+  plt.imshow(train_x[0].reshape((28, 28)), cmap=cm.Greys_r)
   # plt.show()
   plt.savefig('a.png')
 
